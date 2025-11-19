12fffbca75527b6685a66a77d11ccbef4f54312d
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-19 11:05:10 -0300

    Agrego inyección de dependencias para servicios y repositorios de Afiliado y ObraSocial

--- src/modules/pacientes/application/ports/afiliado-repository.interface.ts
+++ src/modules/pacientes/application/ports/afiliado-repository.interface.ts
@@ -1,5 +1,7 @@
 import { Afiliado } from "../../domain/value-objects/afiliado.vo";
 
+export const AFILIADO_REPOSITORIO = Symbol('AFILIADO_REPOSITORIO');
+
 export interface IAfiliadoRepository {
     modificar(afiliado: Afiliado): boolean;
     obtener(numeroAfiliado: string): Afiliado | null;
--- src/modules/pacientes/application/ports/obra-social-service.interface.ts
+++ src/modules/pacientes/application/ports/obra-social-service.interface.ts
@@ -1,5 +1,7 @@
 import { ObraSocial } from "../../domain/entities/obra-social.entity";
 
+export const OBRASOCIAL_SERVICIO = Symbol('OBRASOCIAL_SERVICIO');
+
 export interface IObraSocialService {
   buscar(id: string): ObraSocial | null;
   modificar(obraSocial: ObraSocial): boolean;
