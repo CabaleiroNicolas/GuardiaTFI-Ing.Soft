d5c1e456c62c30efefc0b48babe5275b1a3ae140
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-19 11:07:21 -0300

    Agrego vista para registrar ingresos, más configuración para usar motor Handlebars

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { UrgenciasModule } from './modules/urgencias/urgencias.module';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
+    UrgenciasModule,
     ConfigModule.forRoot({ isGlobal: true })
   ],
   controllers: [AppController],
--- src/modules/urgencias/urgencias.module.ts
+++ src/modules/urgencias/urgencias.module.ts
@@ -3,25 +3,31 @@ import { IngresoService } from './application/services/ingreso.service';
 import { PacienteService } from '../pacientes/application/services/paciente.service';
 import { INGRESO_REPOSITORIO } from './application/ports/ingreso-repository.interface';
 import { IngresoRepositoryMock } from 'test/mocks/ingreso-repository.mock';
-// import { PACIENTE_REPOSITORIO } from './application/ports/paciente-repository.interface';
 import { PacienteRepositoryMock } from 'test/mocks/paciente-repository.mock';
 import { INGRESO_SERVICIO } from './application/ports/ingreso-service.interface';
-// import { PACIENTE_SERVICIO } from './application/ports/paciente-service.interface';
 import { ENFERMERA_REPOSITORIO } from './application/ports/enfermera-repository.interface';
 import { ENFERMERA_SERVICE } from './application/ports/enfermera-service.interface';
 import { EnfermeraService } from './application/services/enfermera.service';
 import { EnfermeraRepositoryMock } from 'test/mocks/enfermera-repository.mock';
+import { PACIENTE_REPOSITORIO } from '../pacientes/application/ports/paciente-repository.interface';
+import { PACIENTE_SERVICIO } from '../pacientes/application/ports/paciente-service.interface';
+import { UrgenciasController } from './presentation/controllers/urgencias.controller';
+import { OBRASOCIAL_REPOSITORIO } from '../pacientes/application/ports/obra-social-repository.interface';
+import { ObraSocialRepositoryMock } from 'test/mocks/obra-social-repository.mock';
+import { AFILIADO_REPOSITORIO } from '../pacientes/application/ports/afiliado-repository.interface';
+import { AfiliadoRepositoryMock } from 'test/mocks/afiliado-repository.mock';
 
 @Module({
+    controllers: [UrgenciasController],
     providers: [
         {
             provide: INGRESO_SERVICIO,
             useClass: IngresoService,
         },
-        // {
-        //     provide: PACIENTE_SERVICIO,
-        //     useClass: PacienteService,
-        // },
+        {
+            provide: PACIENTE_SERVICIO,
+            useClass: PacienteService,
+        },
         {
             provide: ENFERMERA_SERVICE,
             useClass: EnfermeraService,
@@ -30,15 +36,23 @@ import { EnfermeraRepositoryMock } from 'test/mocks/enfermera-repository.mock';
             provide: INGRESO_REPOSITORIO,
             useClass: IngresoRepositoryMock, // Cambiar por IngresoRepositoryImpl cuando se implemente la real
         },
-        // {
-        //     provide: PACIENTE_REPOSITORIO,
-        //     useClass: PacienteRepositoryMock, // Cambiar por PacienteRepositoryImpl cuando se implemente la real
-        // },
-        // {
-        //     provide: ENFERMERA_REPOSITORIO,
-        //     useClass: EnfermeraRepositoryMock, // Cambiar por EnfermeraRepositoryImpl cuando se implemente la real
-        // }
+        {
+            provide: PACIENTE_REPOSITORIO,
+            useClass: PacienteRepositoryMock, // Cambiar por PacienteRepositoryImpl cuando se implemente la real
+        },
+        {
+            provide: ENFERMERA_REPOSITORIO,
+            useClass: EnfermeraRepositoryMock, // Cambiar por EnfermeraRepositoryImpl cuando se implemente la real
+        },
+        {
+          provide: OBRASOCIAL_REPOSITORIO,
+          useClass: ObraSocialRepositoryMock,
+        },
+        {
+          provide: AFILIADO_REPOSITORIO,
+          useClass: AfiliadoRepositoryMock,
+        }
     ],
-    exports: [IngresoService, PacienteService, EnfermeraService],
+    exports: [INGRESO_SERVICIO, PACIENTE_SERVICIO, ENFERMERA_SERVICE]
 })
 export class UrgenciasModule { }
