b01587b7f138ce55fda08016ff2ca3174e2bcb5b
nicolas cabaleiro <nicocabaleiro2002@gmail.com>
2025-12-01 19:12:55 -0300

    Arreglo match de nivel de emergencia

--- src/modules/urgencias/domain/value-objects/nivel-emergencia.enum.ts
+++ src/modules/urgencias/domain/value-objects/nivel-emergencia.enum.ts
@@ -13,7 +13,7 @@ export class NivelEmergenciaHelper {
     switch (nivelEmergenciaStr) {
       case 'Critico':
         return NivelEmergencia.CRITICO;
-      case 'CRITICO':
+      case 'CRITICA':
         return NivelEmergencia.CRITICO;
       case 'Emergencia':
         return NivelEmergencia.EMERGENCIA;
