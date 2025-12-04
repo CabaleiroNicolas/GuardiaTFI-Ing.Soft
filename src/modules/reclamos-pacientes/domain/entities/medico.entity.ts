7d6fb6bd629190bfa0bee13b089fbe193a5ac37f
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-12-04 13:09:21 -0300

    Agrego clase Medico

---
+++ src/modules/reclamos-pacientes/domain/entities/medico.entity.ts
@@ -0,0 +1,34 @@
+import { User } from "src/modules/user/domain/user.entity";
+import { UserRole } from "src/modules/user/domain/value-objects/user-role.enum";
+
+export class Medico extends User {
+  
+  private cuil: string;
+  private apellido: string;
+  private nombre: string;
+  private matricula: string;
+
+  constructor(userId: number, email: string, password: string, role: UserRole, cuil: string, apellido: string, nombre: string, matricula: string) {
+      super({userId, email, password, role});
+      this.cuil = cuil;
+      this.apellido = apellido;
+      this.nombre = nombre;
+      this.matricula = matricula;
+  }
+
+  getCuil(): string {
+    return this.cuil;
+  }
+
+  getApellido(): string {
+    return this.apellido;
+  }
+    
+  getNombre(): string {
+    return this.nombre;
+  }
+
+  getMatricula(): string {
+    return this.matricula;
+  }
+}
