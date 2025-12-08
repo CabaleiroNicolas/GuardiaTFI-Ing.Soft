b0005d0710d125551d0d761ea11d1f0a064b002f
nicolas cabaleiro <nicocabaleiro2002@gmail.com>
2025-12-08 01:03:45 -0300

    Habilito log para cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule,
     { logger: ['error', 'warn', 'log'], }
   );
- 
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
@@ -16,13 +16,28 @@ async function bootstrap() {
   const allowedOrigin = process.env.CORS_ORIGIN || '*';
 
   app.enableCors({
-    origin: allowedOrigin, 
+    origin: (requestOrigin, callback) => {
+      if (!requestOrigin) {
+        return callback(null, true);
+      }
+      console.log(`[CORS] Request desde: '${requestOrigin}'`);
+      console.log(`[CORS] Esperado:      '${allowedOrigin}'`);
+
+      const cleanRequest = requestOrigin.replace(/\/$/, ''); // Quita barra final si existe
+      const cleanAllowed = (allowedOrigin || '').replace(/\/$/, ''); // Quita barra final si existe
+
+      if (cleanRequest === cleanAllowed) {
+        callback(null, true);
+      } else {
+        console.warn(`[CORS] ⛔ Bloqueado. No coinciden.`);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  console.log(allowedOrigin)
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
