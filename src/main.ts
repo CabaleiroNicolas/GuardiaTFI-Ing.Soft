25492bd7652e80b3690ea40667c60c551597a140
nicolas cabaleiro <nicocabaleiro2002@gmail.com>
2025-12-08 00:56:48 -0300

    agrego log de debug

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
+  console.log(allowedOrigin)
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
