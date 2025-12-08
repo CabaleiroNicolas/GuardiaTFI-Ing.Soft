d056c7bc01fa650fb06801fe18e28db9879a8af9
nicolas cabaleiro <nicocabaleiro2002@gmail.com>
2025-12-08 01:15:53 -0300

    cors al iniciar app

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,
-    { logger: ['error', 'warn', 'log'], }
-  );
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    },
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -15,11 +19,6 @@ async function bootstrap() {
 
   const allowedOrigin = process.env.CORS_ORIGIN || '*';
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
