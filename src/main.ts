c3c93120f16573602a6a2866b36a53f24b3910e9
nicolas cabaleiro <nicocabaleiro2002@gmail.com>
2025-12-08 00:35:12 -0300

    Actualizo main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule,
     { logger: ['error', 'warn', 'log'], }
   );
   app.enableCors();
-  // Archivos públicos (css, imágenes, JS)
-  app.useStaticAssets(join(process.cwd(), 'public'));
-
-  // Configurar directorio de vistas
-  app.setBaseViewsDir(join(process.cwd(), 'views'));
-
-  // Establecer Handlebars como motor de vistas
-  app.setViewEngine('hbs');
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
